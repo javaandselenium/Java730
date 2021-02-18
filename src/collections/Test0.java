1e90a6888904876f75b64432642aaded59f7bab1
QSP <QSP@DESKTOP-EVK9GMC>
2021-02-18 08:30:02 +0530

    Collections-1

---
+++ src/collections/Test0.java
@@ -0,0 +1,18 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class Test0 {
+
+	public static void main(String[] args) {
+		ArrayList a=new ArrayList();
+		a.add(10);
+		a.add(20.40);
+		a.add('A');
+		a.add("hello");
+		
+		System.out.println(a.size());
+		
+	}
+
+}
